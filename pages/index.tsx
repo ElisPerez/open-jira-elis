d827f8af2c52f425dcec70a1b1cbe5be4e9db290
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-20 17:44:25 -0500

    Cards added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
-import { Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hello World</Typography>
+    <Layout title='Home | OpenJira EA'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title='Not started' />
+            <CardContent>
+              {/* add */}
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title='In progress' />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title='Completed' />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
-  )
+  );
 };
 
 export default HomePage;
