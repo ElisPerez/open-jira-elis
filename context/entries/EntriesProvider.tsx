02877896e50aaab365be8916290653da5faf54f6
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-21 11:25:34 -0500

    implement Entries context

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,14 +1,34 @@
 import { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Entry } from '../../interfaces';
+
 import { EntriesContext, entriesReducer } from './';
+import { Entry } from '../../interfaces';
 
 export interface EntriesState {
   entries: Entry[];
 }
 
 const ENTRIES_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      createdAt: Date.now(),
+      description: 'Nostrud proident dolore eu reprehenderit exercitation est.',
+      status: 'pending',
+    },
+    {
+      _id: uuidv4(),
+      createdAt: Date.now() - 1000000,
+      description: 'Consequat irure do consectetur nulla consequat laboris esse.',
+      status: 'in-progress',
+    },
+    {
+      _id: uuidv4(),
+      createdAt: Date.now() - 100000,
+      description: 'Cillum fugiat sunt Lorem non commodo ad enim proident laboris amet dolore.',
+      status: 'finished',
+    },
+  ],
 };
 
 interface Props {
