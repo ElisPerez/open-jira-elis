466899c92c5b3c7a6f5f04c3b4be3e47a0768181
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-30 12:10:22 -0500

    POST Entry

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -9,6 +9,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     case 'GET':
       return getEntries(res);
 
+    case 'POST':
+      return postEntry(req, res);
+
     default:
       return res.status(400).json({ message: 'Endpoint not found Elis' });
   }
@@ -23,4 +26,31 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   res.status(200).json(entries);
+};
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description = '' } = req.body;
+  // console.log(req.body);
+
+  const newEntry = new EntryModel({
+    createdAt: new Date(),
+    description,
+  });
+
+  try {
+    await db.connet();
+
+    // Interaction with the database
+    await newEntry.save();
+
+    await db.disconnect();
+
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+
+    console.log('an error here Elis:', error);
+
+    return res.status(500).json({ message: 'Something went wrong, check the server console' });
+  }
 };
