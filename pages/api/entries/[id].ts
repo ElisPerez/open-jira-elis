963426cee1cd83e5b2e12edfd30aa84b89cd75e1
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-02 16:02:43 -0500

    [id].ts Update entry by id

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { EntryModel, IEntry } from '../../../models';
+
+type Data = { message: string } | IEntry;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: `ID: ${id} is not valid!` });
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res);
+
+    case 'GET':
+      return res.status(500).json({ message: 'An Error here Elis' });
+
+    default:
+      return res.status(400).json({ message: 'Method is not valid! Elis' });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    await db.connet();
+
+    const { id } = req.query;
+
+    const entryToUpdate = await EntryModel.findById(id);
+
+    if (!entryToUpdate) {
+      await db.disconnect();
+      return res.status(400).json({ message: `Entry with ID ${id} isn't exist` });
+    }
+
+    const { description = entryToUpdate.description, status = entryToUpdate.status } = req.body;
+
+    const updatedEntry = await EntryModel.findByIdAndUpdate(
+      id,
+      { description, status },
+      { runValidators: true, new: true }
+    );
+
+    res.status(200).json(updatedEntry!);
+  } catch (error) {
+    console.error('An error here Elis:', error);
+  }
+};
