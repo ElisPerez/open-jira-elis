cb5cc9baea9f27c1c4e0bd5916bda8598bd3e16d
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-05 14:22:11 -0500

    middleware basic: matcher: /api/entries/:path

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,12 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
-  // console.log({ req: req.nextUrl });
+  console.log('Passed through "/api/entries/:path/"');
+  console.log({ path: req.nextUrl.pathname });
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    // '/api/entries',
-    '/api/entries/:path*',
-  ],
+  matcher: '/api/entries/:path/',
 };
